7e3a8cdecebc630bbacd334dcf7bd867e2aee03a
sunhaojie <sunshine2285@163.com>
2019-10-31 22:06:24 +0800

    update

--- pces/src/main/java/com/ouc/pces/controller/HelloController.java
+++ pces/src/main/java/com/ouc/pces/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.ouc.pces.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+@ApiIgnore
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
+//    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
-        return "Hello SpringBoot（git）";
+        return "Hello SpringBoot";
     }
 }
