2ae1a23f9496f539443d5434ceb0e06719e598af
sunhaojie <sunshine2285@163.com>
2019-11-01 13:47:30 +0800

    Create RegisterController.java

---
+++ pces/src/main/java/com/ouc/pces/controller/RegisterController.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019
+ * FileName: RegisterController.java
+ * @Author: 孙浩杰
+ * @LastModified:2019-10-31 22:15:29
+ */
+
+package com.ouc.pces.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "注册", tags = "注册接口")
+@CrossOrigin(origins = "*")
+@RestController
+public class RegisterController {
+
+}
