df50902e7e3dd2362586d46a8d62300d99df395c
sunhaojie <sunshine2285@163.com>
2019-11-01 15:56:47 +0800

    Create TeacherService.java

---
+++ pces/src/main/java/com/ouc/pces/service/TeacherService.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019
+ * FileName: TeacherService.java
+ * @Author: 孙浩杰
+ * @LastModified:2019-11-01 15:50:20
+ */
+
+package com.ouc.pces.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeacherService {
+}
