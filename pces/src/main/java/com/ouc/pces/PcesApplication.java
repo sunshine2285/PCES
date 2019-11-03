53e4421b567a78e9da8a0b5ee4960bd896b4a94e
sunhaojie <sunshine2285@163.com>
2019-11-03 21:00:17 +0800

    配置生成jar

--- pces/src/main/java/com/ouc/pces/PcesApplication.java
+++ pces/src/main/java/com/ouc/pces/PcesApplication.java
@@ -4,16 +4,23 @@ import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrap
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @EnableSwaggerBootstrapUI
 @MapperScan("com.ouc.pces.mapper")
 @SpringBootApplication
-public class PcesApplication {
+public class PcesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PcesApplication.class, args);
     }
 
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
