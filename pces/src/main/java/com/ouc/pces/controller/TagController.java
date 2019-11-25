31ec6eeef0dee91a461536d69495fb3bead8860f
tmyfxjy <tmyyes@163.com>
2019-11-25 21:36:36 +0800

    update Tag service

--- pces/src/main/java/com/ouc/pces/controller/TagController.java
+++ pces/src/main/java/com/ouc/pces/controller/TagController.java
@@ -1,9 +1,8 @@
-
-
 package com.ouc.pces.controller;
 
 import com.ouc.pces.entity.College;
 import com.ouc.pces.entity.Major;
+import com.ouc.pces.entity.Tag;
 import com.ouc.pces.entity.TagType;
 import com.ouc.pces.service.TagService;
 import io.swagger.annotations.Api;
@@ -21,18 +20,17 @@ import java.util.ArrayList;
 public class TagController {
     @Autowired
     TagService tagService;
-//
-//    @ApiOperation(value = "所有标签", notes = "获取所有可添加的标签")
-//    @GetMapping(value = "/tag", produces = "application/json")
-//    public ArrayList<TagType> getAllTagType() {
-//        return tagService.selectAll();
-//    }
-//
-//    @ApiOperation(value = "某学院所有专业", notes = "根据学院ID获取该学院所有专业信息")
-//    @GetMapping(value = "/{collegeId}", produces = "application/json")
-//    public ArrayList<Major> getMajorsByCollegeId(@ApiParam(value = "学院ID", required = true)
-//                                                 @PathVariable(value = "collegeId") int collegeId) {
-//        return majorService.selectByCollegeId(collegeId);
-//    }
 
+    @ApiOperation(value = "所有标签", notes = "获取所有可添加标签")
+    @GetMapping(value = "", produces = "application/json")
+    public ArrayList<TagType> getAllTagType() {
+        return tagService.selectAll();
+    }
+
+    @ApiOperation(value = "某课程所有已添加标签", notes = "根据课程ID获取所有已添加标签")
+    @GetMapping(value = "/{courseId}", produces = "application/json")
+    public ArrayList<Tag> getTagByCourseId(@ApiParam(value = "课程ID", required = true)
+                                                 @PathVariable(value = "courseId") int courseId) {
+        return tagService.selectTagsByCourseId(courseId);
+    }
 }
--- pces/src/main/java/com/ouc/pces/service/TagService.java
+++ pces/src/main/java/com/ouc/pces/service/TagService.java
@@ -1,8 +1,21 @@
 package com.ouc.pces.service;
 
+import com.ouc.pces.entity.TagType;
+import com.ouc.pces.mapper.TagMapper;
+import com.ouc.pces.mapper.TagTypeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class TagService {
+    @Autowired
+    TagMapper tagMapper;
+    @Autowired
+    TagTypeMapper tagTypeMapper;
 
+    public ArrayList<TagType> sellectAll(){
+        return new ArrayList<>();
+    }
 }
