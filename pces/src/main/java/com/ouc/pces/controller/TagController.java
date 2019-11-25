c445a454b006926afc9785270477738779115ff8
tmyfxjy <tmyyes@163.com>
2019-11-25 20:41:04 +0800

    code modify

--- pces/src/main/java/com/ouc/pces/controller/TagController.java
+++ pces/src/main/java/com/ouc/pces/controller/TagController.java
@@ -1,10 +1,18 @@
+
+
 package com.ouc.pces.controller;
 
+import com.ouc.pces.entity.College;
+import com.ouc.pces.entity.Major;
+import com.ouc.pces.entity.TagType;
+import com.ouc.pces.service.TagService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Api(value = "标签", tags = "标签接口")
 @CrossOrigin(origins = "*")
@@ -12,5 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tag")
 public class TagController {
     @Autowired
-    TagController tagController;
+    TagService tagService;
+//
+//    @ApiOperation(value = "所有标签", notes = "获取所有可添加的标签")
+//    @GetMapping(value = "/tag", produces = "application/json")
+//    public ArrayList<TagType> getAllTagType() {
+//        return tagService.selectAll();
+//    }
+//
+//    @ApiOperation(value = "某学院所有专业", notes = "根据学院ID获取该学院所有专业信息")
+//    @GetMapping(value = "/{collegeId}", produces = "application/json")
+//    public ArrayList<Major> getMajorsByCollegeId(@ApiParam(value = "学院ID", required = true)
+//                                                 @PathVariable(value = "collegeId") int collegeId) {
+//        return majorService.selectByCollegeId(collegeId);
+//    }
+
 }
