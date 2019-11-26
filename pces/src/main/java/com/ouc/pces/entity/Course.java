481e8e17abdc2db3c36f370d81e46bec174748fc
sunhaojie <sunshine2285@163.com>
2019-11-26 12:46:04 +0800

    update entity

--- pces/src/main/java/com/ouc/pces/entity/Course.java
+++ pces/src/main/java/com/ouc/pces/entity/Course.java
@@ -1,4 +1,100 @@
 package com.ouc.pces.entity;
 
+
 public class Course {
+
+  private long id;
+  private String name;
+  private String teacher;
+  private String schoolTime;
+  private String campus;
+  private String courseType;
+  private String remark;
+  private String introduction;
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", teacher='" + teacher + '\'' +
+                ", schoolTime='" + schoolTime + '\'' +
+                ", campus='" + campus + '\'' +
+                ", courseType='" + courseType + '\'' +
+                ", remark='" + remark + '\'' +
+                ", introduction='" + introduction + '\'' +
+                '}';
+    }
+
+    public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getTeacher() {
+    return teacher;
+  }
+
+  public void setTeacher(String teacher) {
+    this.teacher = teacher;
+  }
+
+
+  public String getSchoolTime() {
+    return schoolTime;
+  }
+
+  public void setSchoolTime(String schoolTime) {
+    this.schoolTime = schoolTime;
+  }
+
+
+  public String getCampus() {
+    return campus;
+  }
+
+  public void setCampus(String campus) {
+    this.campus = campus;
+  }
+
+
+  public String getCourseType() {
+    return courseType;
+  }
+
+  public void setCourseType(String courseType) {
+    this.courseType = courseType;
+  }
+
+
+  public String getRemark() {
+    return remark;
+  }
+
+  public void setRemark(String remark) {
+    this.remark = remark;
+  }
+
+
+  public String getIntroduction() {
+    return introduction;
+  }
+
+  public void setIntroduction(String introduction) {
+    this.introduction = introduction;
+  }
+
 }
--- pces/src/main/java/com/ouc/pces/entity/Tag.java
+++ pces/src/main/java/com/ouc/pces/entity/Tag.java
@@ -1,7 +1,47 @@
-package com.ouc.pces.entity;
+package com.sample;
 
-import org.springframework.stereotype.Repository;
 
-@Repository
 public class Tag {
+
+  private long courseId;
+  private long tagId;
+  private long num;
+  private long id;
+
+
+  public long getCourseId() {
+    return courseId;
+  }
+
+  public void setCourseId(long courseId) {
+    this.courseId = courseId;
+  }
+
+
+  public long getTagId() {
+    return tagId;
+  }
+
+  public void setTagId(long tagId) {
+    this.tagId = tagId;
+  }
+
+
+  public long getNum() {
+    return num;
+  }
+
+  public void setNum(long num) {
+    this.num = num;
+  }
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
 }
