622a725d919e923d889b73d78824ca1e61bd61af
sunhaojie <sunshine2285@163.com>
2019-12-01 17:23:12 +0800

    添加根据专业查询课程接口

--- pces/src/main/java/com/ouc/pces/mapper/CourseMapper.java
+++ pces/src/main/java/com/ouc/pces/mapper/CourseMapper.java
@@ -10,4 +10,5 @@ public interface CourseMapper {
     public ArrayList<Course> selectByCourseType(String courseType);
     public Course selectById(int id);
     public ArrayList<Course> selectByKeyWords(String KeyWords);
+    public ArrayList<Course> selectByMajorId(int majorId);
 }
