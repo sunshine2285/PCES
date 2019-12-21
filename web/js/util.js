9f97817a4e063625f1af494c694c2a1f20d36c29
Song <54357504+song-zk@users.noreply.github.com>
2019-12-21 19:23:29 +0800

    Update util.js

--- web/js/util.js
+++ web/js/util.js
@@ -2,19 +2,19 @@ let baseUrl = "http://sunshine-sun.cn:2285/";
 
 function request (url, method, data, callback) {
     fetch(url, {
-            headers: {
-                'content-type': 'application/json'
-            },
-            mode: 'cors',
-            method: method,
-            body: data === null ? null : JSON.stringify(data)
-        })
+        headers: {
+            'content-type': 'application/json'
+        },
+        mode: 'cors',
+        method: method,
+        body: data === null ? null : JSON.stringify(data)
+    })
         .then(response => response.json())
         .then(result => {
             callback(result);
         }).catch(error => {
-            console.error(error);
-        });
+        console.error(error);
+    });
 }
 
 function setCookie (key, value) {
@@ -28,5 +28,55 @@ function getCookie (key) {
 }
 
 function removeCookie(key) {
-    document.cookie = key + "=;"; 
+    document.cookie = key + "=;";
 }
+
+//回掉函数
+// function loginCallBack (result) {
+//     console.log('result :', result);
+// }
+
+//Get请求
+// request("college", "GET", null, loginCallBack);
+
+//Post请求
+// var url = "login/student";
+// let userData = {
+//     userId: document.getElementById("userId").value,
+//     password: document.getElementById("password").value
+// }
+// request(url, userData, loginCallBack);
+
+
+function formatTemplate(dta, tmpl, format) {
+    return tmpl.replace(/{(\w+)}/g, function(m1, m2) {
+        if (!m2)
+            return "";
+        return (format && format[m2])? format[m2](dta[m2]) : dta[m2];
+    });
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
+    return (false);
+}
+// function formatTemplate(data, tmpl) {
+//     return tmpl.replace(/{(\w+)}/g, function(m1, m2) {
+//         if (!m2)
+//             return "";
+//         return data[m2];
+//     });
+// }
+
+// var format = {
+//         name: function(x) {
+//             return x ;
+//         }
+//     };
